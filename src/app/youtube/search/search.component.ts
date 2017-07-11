16a6c0e63578df06b2a4f823026aa01baafa6fd2
Marek Krawczyk <marek@headchannel.co.uk>
2017-07-12 00:36:55 +0200

    home

--- src/app/youtube/search/search.component.ts
+++ src/app/youtube/search/search.component.ts
@@ -7,12 +7,17 @@ import { SearchResult } from '../../youtube/model/search-result.model';
   styleUrls: ['./search.component.css']
 })
 export class SearchComponent implements OnInit {
-result: SearchResult[];
-loading: boolean;
+  results: SearchResult[];
+  loading: boolean;
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  updateResults(results: SearchResult[]): void {
+    this.results = results;
+    // console.log("results:", this.results); // uncomment to take a look
   }
 
 }
--- src/app/youtube/services/youtube-search.service.ts
+++ src/app/youtube/services/youtube-search.service.ts
@@ -3,7 +3,7 @@ import { Http, Response } from '@angular/http';
 import { Observable } from 'rxjs/Observable';
 import { SearchResult } from '../../youtube/model/search-result.model';
 import 'rxjs/Rx';
-export const YOUTUBE_API_KEY: string = "XXX_YOUR_KEY_HERE_XXX";
+export const YOUTUBE_API_KEY: string = "AIzaSyAPGY72uIBkTS8N21GD4DDG6nck0UWMtAw";
 export const YOUTUBE_API_URL: string = "https://www.googleapis.com/youtube/v3/search";
 
 @Injectable()
